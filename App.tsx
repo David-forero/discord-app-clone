ffb4b62733e548a0822b81e429af71f7170cbd2c
David-forero <david.forero1813@gmail.com>
2022-04-23 15:41:32 -0400

    iniciando y creando interfaz de signup

--- App.tsx
+++ App.tsx
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-gesture-handler";
+import useCachedResources from "./src/hooks/useCachedResources";
+import useColorScheme from "./src/hooks/useColorScheme";
+import Navigation from "./src/navigation";
+import React, { useEffect } from "react";
+import { OverlayProvider, Chat, ChannelList, Channel, MessageList, MessageInput } from "stream-chat-expo";
+import { StreamChat } from "stream-chat";
+import AuthContext from './src/contexts/AuthContext';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+const API_KEY = "ea2zqy9t4mfu";
+const client = StreamChat.getInstance(API_KEY);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
+  useEffect(() => {
+    
+  
+    return () => {
+      client.disconnectUser();
+    }
+  }, [])
+  
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+       <AuthContext>
+          <OverlayProvider>
+            <Chat client={client}>
+              <Navigation colorScheme={colorScheme} />
+              {/* {!selectedChannel ? (
+                <ChannelList onSelect={onChannelSelected} />
+              ) : (
+                <Channel channel={selectedChannel}>
+                  <Text style={{margin: 50}} onPress={() => setSelectedChannel(null)}>Go Back</Text>
+                  <MessageList/>
+                  <MessageInput/>
+                </Channel>
+              )} */}
+            </Chat>
+          </OverlayProvider>
+          <StatusBar style="light" />
+       </AuthContext>
       </SafeAreaProvider>
     );
   }
